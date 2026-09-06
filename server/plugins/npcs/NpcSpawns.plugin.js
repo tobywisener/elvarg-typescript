598247c803adda31515d89c2c73d23883ace28e7
toby <tobywisener@googlemail.com>
2026-09-06 11:48:57 +0100

    simplify JSON definition source registration

--- server/plugins/npcs/NpcSpawns.plugin.js
+++ server/plugins/npcs/NpcSpawns.plugin.js
@@ -1,18 +1,6 @@
-const fs = require("fs");
-const path = require("path");
-
-const SPAWN_FILE = path.join(process.cwd(), "data", "definitions", "npc_spawns.json");
-
 module.exports = {
   name: "NpcSpawns",
   register(api) {
-    api.registerDefinitionSource("npc_spawns", {
-      name: "npc-spawns",
-      load() {
-        const spawns = JSON.parse(fs.readFileSync(SPAWN_FILE, "utf8"));
-        if (!Array.isArray(spawns)) throw new Error(`${SPAWN_FILE} must contain an array`);
-        return spawns;
-      },
-    });
+    api.registerDefinitionSource("npc_spawns", "data/definitions/npc_spawns.json");
   },
 };
